3db2ef6837118d62f28cbb7585f9e4ca0da07c4f
Jon Meyers <jonathon.d.meyers@gmail.com>
2025-01-15 21:50:09 +1100

    fix auth redirect flow

--- app/actions.ts
+++ app/actions.ts
@@ -50,7 +50,7 @@ export const signInAction = async (formData: FormData) => {
     return encodedRedirect("error", "/sign-in", error.message);
   }
 
-  return redirect("/protected");
+  return redirect("/users");
 };
 
 export const forgotPasswordAction = async (formData: FormData) => {
--- app/layout.tsx
+++ app/layout.tsx
@@ -45,7 +45,9 @@ export default function RootLayout({
                   {!hasEnvVars ? <EnvVarWarning /> : <HeaderAuth />}
                 </div>
               </nav>
-              <div className="flex flex-col gap-20 p-5 w-full">{children}</div>
+              <div className="flex flex-col gap-20 p-5 w-full items-center">
+                {children}
+              </div>
 
               <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
                 <p>
